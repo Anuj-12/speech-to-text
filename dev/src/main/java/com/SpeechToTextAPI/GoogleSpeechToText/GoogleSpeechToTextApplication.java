e5b868d7c3468e2f7d78e5051b716db70eade10a
Anuj Tejas <anujtjs@gmail.com>
2024-08-05 18:08:17 +0530

    Added maven files

---
+++ dev/src/main/java/com/SpeechToTextAPI/GoogleSpeechToText/GoogleSpeechToTextApplication.java
@@ -0,0 +1,13 @@
+package com.SpeechToTextAPI.GoogleSpeechToText;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSpeechToTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSpeechToTextApplication.class, args);
+	}
+
+}
